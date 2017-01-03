3fab077636007042f84e5c10213ca1aa67cda0a9
Raymond Sapida <ray.sapida@gmail.com>
2017-01-02 21:01:35 -0800

    Update proxy test to show that the owner is the first account set

--- test/proxy.js
+++ test/proxy.js
@@ -1,12 +1,11 @@
 contract('Proxy', function(accounts) {
-  it("should have an owner", function() {
+  it("should have an owner thats set to the first account", function() {
     var proxy = Proxy.deployed();
 
-    console.log(proxy)
-    return proxy.owner
-    // return meta.getBalance.call(accounts[0]).then(function(balance) {
-    //   assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-    // });
+    return proxy.owner.call().then((data) => {
+      assert.equal(data, accounts[0], "The owner is the first accounts");
+    })
+
   });
 
 });
