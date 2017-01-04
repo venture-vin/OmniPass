35c45516f2d943b534ceef8e180495ff9c4eda14
Raymond Sapida <ray.sapida@gmail.com>
2017-01-04 15:00:10 -0800

    Write a test case for multiple identities and check if they persist

--- test/user.js
+++ test/user.js
@@ -30,4 +30,46 @@ contract('User', function(accounts) {
 
   });
 
+  it("creates multiple identities that can be accessed by account address", function() {
+    var proxy = Proxy.deployed();
+    var testName = 'test_name'
+    var testEmail = 'test@email.com'
+    var userName = 'user_name'
+    var userEmail = 'user@email.com'
+
+    return proxy.createIdentity(accounts[1], testName, testEmail)
+      .then((data) => {
+
+        return proxy.createIdentity(accounts[2], userName, userEmail).then((data) => {
+
+        })
+          .then(() => {
+            return proxy.getUsername.call(accounts[1])
+          })
+          .then((username) => {
+
+            assert.equal(username, testName, "The function getUsername returns " + testName);
+            return proxy.getEmail.call(accounts[1])
+
+          })
+          .then((email) => {
+            assert.equal(email, testEmail, "The function getEmail returns " + testEmail);
+
+            return proxy.getUsername.call(accounts[2])
+
+          })
+          .then((username) => {
+
+            assert.equal(username, userName, "The function getUsername returns " + testName);
+            return proxy.getEmail.call(accounts[2])
+
+          })
+          .then((email) => {
+            assert.equal(email, userEmail, "The function getEmail returns " + testEmail);
+          });
+
+      });
+
+  });
+
 });
